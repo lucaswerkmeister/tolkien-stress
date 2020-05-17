282301e6a89395cd389c0a36023c31eae5f6946c
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-05-17 19:05:25 +0200

    Initial commit

---
+++ web.mjs
@@ -0,0 +1,33 @@
+import analyse from './index.mjs';
+
+function display(word, language) {
+    const { syllableBreaks, stressedSyllable } = analyse(word, language);
+    const span = document.createElement('span');
+    span.classList.add('word', `word-${language}`);
+    for (let i = 0; i < syllableBreaks.length - 1; i++) {
+        const start = syllableBreaks[i], end = syllableBreaks[i+1];
+        const syllable = document.createElement('span');
+        syllable.classList.add('syllable');
+        syllable.textContent = word.substring(start, end);
+        if (i === stressedSyllable) {
+            syllable.classList.add('syllable-stressed');
+        }
+        span.append(syllable);
+    }
+    return span;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const wordInput = document.getElementById('word-input');
+    function updateWord() {
+        const language = document.querySelector('input[name=language]:checked').value;
+        const newWord = display(wordInput.value, language);
+        newWord.id = 'word';
+        document.getElementById('word').replaceWith(newWord);
+    }
+    wordInput.addEventListener('input', updateWord);
+    document.querySelectorAll('input[name=language]').forEach(languageInput => {
+        languageInput.addEventListener('input', updateWord);
+    });
+    updateWord();
+});
