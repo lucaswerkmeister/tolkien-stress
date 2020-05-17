3a4710abf9d199022cc74623ec5c9fa8efe1e3ae
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-05-17 20:46:56 +0200

    Specify test cases more easily
    Directly specifying indices is cumbersome and makes the tests
    unreadable. Invent a pseudo-syntax for marking stress and syllables
    instead and construct the real test cases from that.

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -3,10 +3,23 @@ import 'chai/register-expect.js';
 
 describe('analyse', () => {
 	const testCases = [
-		['Aman', 'Sindarin', [0, 1, 4], 0],
+		['A|man', 'Sindarin'],
+		['e|RES|së|a', 'Quenya'],
 	];
-	for (const [word, language, expectedSyllableBreaks, expectedStressedSyllable] of testCases) {
-		it(`correctly analyses ${word} in ${language}`, () => {
+	for (const [testCase, language] of testCases) {
+		let word = '';
+		const expectedSyllableBreaks = [0];
+		let expectedStressedSyllable = null;
+		const syllables = testCase.split('|');
+		for (let [i, syllable] of syllables.entries()) {
+			word += syllable.toLowerCase();
+			expectedSyllableBreaks.push(word.length);
+			if (syllable.toUpperCase() === syllable) {
+				expectedStressedSyllable = i;
+			}
+		}
+		word = word.replace(/^./, (char) => char.toUpperCase());
+		it(`correctly analyses ${word} in ${language} (${testCase})`, () => {
 			const { syllableBreaks, stressedSyllable } = analyse(word, language);
 			expect(syllableBreaks).to.eql(expectedSyllableBreaks);
 			expect(stressedSyllable).to.equal(expectedStressedSyllable);
